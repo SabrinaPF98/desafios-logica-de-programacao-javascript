b11517a80a32ee4540e37de94b8b926234919ba2
Sabrina Pereira <sabrina_p.f@hotmail.com>
2025-08-17 10:46:25 -0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 29;
-let palpite = prompt("Escolha um número entre 1 e 30");  // pede o palpite e guarda na variavel
-
-if (palpite == numeroSecreto) {
-    console.log("Isso ai! Você descobriu o número secreto (29)");
-} else {
-    console.log("Que pena! Você errou o número secreto.");
-}
